3f0ff7f45f89c8f8fc2e218fe6054c7b8d7a8d65
Muhamad Gunawan <igunnawan25@gmail.com>
2025-06-19 20:28:52 +0700

    Fix Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/static/Navbar";
+import Footer from "@/components/static/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <main className="bg-gray-100 min-h-screen">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
